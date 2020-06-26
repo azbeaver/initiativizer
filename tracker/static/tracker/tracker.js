d618b97c3dce57ca64917e765892549ab972a7bf
Ava Beaver <zanbeaver@gmail.com>
2020-06-25 22:19:11 -0400

    added function in tracker.js to edit rows by selecting; todo: make edit mode element-specific, leave edit mode

--- tracker/static/tracker/tracker.js
+++ tracker/static/tracker/tracker.js
@@ -1,12 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
+// Creature template
+const creatureTemplate =
+	'<tr class="creature" onclick="edit(this)">\n'
+		+ '\t<td><input type="checkbox" name="num"></td>\n'
+		+ '\t<td><p>Initiative</p></td>\n'
+		+ '\t<td><p>Name</p></td>\n'
+		+ '\t<td><input type="text" class="hp-box"> / <input type="text" class="hp-box"></td>\n'
+		+ '\t<td><input type="text" class="notes-box" placeholder="Notes"></td>\n'
+	+ '</tr>';
 
-	// Template for creating a new creature
-	const template = Handlebars.compile(document.querySelector('#new-creature').innerHTML);
+// Function to put a creature into "edit mode" by clicking on it
+function edit(creature) {
+	console.log(creature);
+	var elements = creature.children;
+
+	for(i = 0; i < elements.length; i++) {
+		let element = elements[i].firstChild;
+		if(element.tagName === 'P') {
+			let text = element.firstChild.textContent;
+			elements[i].innerHTML = '<input type="text" value="' + text + '" >';
+		}
+	}
+}
+
+document.addEventListener('DOMContentLoaded', () => {
 
 	// Add a new creature based on the template when add button clicked
 	document.querySelector('#add').onclick = () => {
-		const creature = template();
-		document.querySelector('#creatures').innerHTML += creature;
+		const creature = creatureTemplate;
+		document.querySelector('#creatures').innerHTML += creature;	
 	};
 
 	// Delete selected creatures when delete button is clicked
@@ -17,5 +38,6 @@ document.addEventListener('DOMContentLoaded', () => {
 				creature.remove();
 			}
 		});
-	};
+	};	
+
 });
