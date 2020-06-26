4a33f2a2ce28265e22e60128f058548493a527d8
Ava Beaver <zanbeaver@gmail.com>
2020-06-26 19:03:48 -0400

    list now sorts with initiative changes; bug: textbox content is lost with sorting

--- README.md
+++ README.md
@@ -3,4 +3,16 @@ written by Ava Beaver
 
 
 This is a personal project of mine made with the goal of learning to make a web app with Django
-and gaining practice with HTML, CSS, and JavaScript
+and gaining practice with HTML, CSS, and JavaScript.
+
+
+This application tracks the initiative, HP, and various notes about each creature in a D&D encounter.
+The list of creatures automatically sorts when an initiative updates. Finally, there are "start"
+and "next round" buttons which highlight which creature's turn it is.
+
+
+Note: because the sorting is done through "bubbling up," more creatures creates a larger overhead.
+This site is intended to be fast and light-weight with encounters with less than 50 creatures.
+
+(Though if you are a DM making an encounter with more than 50 turns per round, time is something
+you have a lot of :wink: )
--- tracker/static/tracker/tracker.js
+++ tracker/static/tracker/tracker.js
@@ -1,8 +1,12 @@
+// ----------------
+// Global Variables
+// ----------------
+
 // Creature template
 const creatureTemplate =
 	'<tr class="creature">\n'
 		+ '\t<td><input type="checkbox"></td>\n'
-		+ '\t<td onclick="edit(this)"><p>Initiative</p></td>\n'
+		+ '\t<td onclick="edit(this)"><p class="initiative">Initiative</p></td>\n'
 		+ '\t<td onclick="edit(this)"><p>Name</p></td>\n'
 		+ '\t<td><input type="text" class="hp-box"> / <input type="text" class="hp-box"></td>\n'
 		+ '\t<td><input type="text" class="notes-box" placeholder="Notes"></td>\n'
@@ -10,40 +14,99 @@ const creatureTemplate =
 
 // Global variable for element being edited
 var elementEditing = null;
-// Global variable indicating when "edit state" is on - false by default
-var isEditing = false;
+
+
+// -----------------
+// Various Functions
+// -----------------
 
 // Function to put an individual into "edit state" by clicking on it
 function edit(element) {
-	// this function only applies when edit state is not on
-	if(isEditing) {
-		return;
-	}
-	
 	stopEditing(elementEditing);
 	elementEditing = element;
 
-	let text = element.firstChild.textContent;
-	element.innerHTML = '<input type="text" value="' + text + '" >';
+	var text = element.firstChild.textContent;
+	element.innerHTML = '<input type="text" class="' + element.firstChild.className + '" value="' + text + '" >';
 	element.onclick = '';
 }
 
+// Function that takes an element out of edit state, including sorting it
 function stopEditing(element) {
 	if(element === null) {
 		return;
 	}
-
-	let text = element.firstChild.value;
-	element.innerHTML = '<p>' + text + '</p>';
+	
+	var text = element.firstChild.value;
+	element.innerHTML = '<p class="' + element.firstChild.className + '">' + text + '</p>';
 	element.onclick = () => {
 		edit(element);
 	};
+
+	if(element.parentElement != null) {
+		sortElement(element.parentElement);
+	}
 }
 
+// Function that sorts a row in the table by bubbling up
+//
+// PRE: table was sorted before this element was changed
+function sortElement(element) {
+	var elementValue = Number(element.querySelector('.initiative').textContent);
+	var up = element.previousElementSibling;
+	if(up != null) {
+		var upValue = Number(up.querySelector('.initiative').textContent);
+	}
+	var down = element.nextElementSibling;
+	if(down != null) {
+		var downValue = Number(down.querySelector('.initiative').textContent);
+	}
+	var temp;
+
+	// Bubble up if greater
+	if(up != null) {
+		while(elementValue > upValue || (!isNaN(elementValue) && isNaN(upValue))) {
+			temp = up.innerHTML;
+			up.innerHTML = element.innerHTML;
+			element.innerHTML = temp;
+
+			up = up.previousElementSibling;
+			if(up === null) {
+				return;
+			}
+			upValue = Number(up.querySelector('.initiative').textContent);
+			element = element.previousElementSibling;
+			console.log(element);
+			elementValue = Number(element.querySelector('.initiative').textContent);
+		}
+	}
+	// Bubble down if less
+	else if(down != null) {
+		while(elementValue < downValue || (isNaN(elementValue) && !isNaN(downValue))) {
+			temp = down.innerHTML;
+			down.innerHTML = element.innerHTML;
+			element.innerHTML = temp;
+
+			down = down.nextElementSibling;
+			if(down === null) {
+				return;
+			}
+			downValue = Number(down.querySelector('.initiative').textContent);
+			element = element.nextElementSibling;
+			elementValue = Number(element.querySelector('.initiative').textContent);
+		}
+	}
+}
+
+// Function that resets the edit states of all elements
 function makeDefaultState() {
 	stopEditing(elementEditing);
 	elementEditing = null;
 }
+
+
+// ---------------------------------------
+// Add button functionality when DOM loads
+// ---------------------------------------
 
 document.addEventListener('DOMContentLoaded', () => {
 	
